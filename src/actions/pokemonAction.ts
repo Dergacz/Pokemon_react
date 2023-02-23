f230310d1b363c949aba312e234c93f62e373430
Stas <sats96dergach@gamil.com>
2023-02-23 14:21:12 +0100

    renamed folders

--- src/actions/pokemonAction.ts
+++ src/actions/pokemonAction.ts
@@ -1,7 +1,8 @@
 import { Dispatch } from '@reduxjs/toolkit';
 import { pokemonSlice } from '../reducers/pokemonSlice';
 import axios from 'axios';
-import { IEvolvesTo, IFetchEvolutionChain, IFetchPokemons, IPokemon, IPokemonSpecies } from '../models/models';
+import { IFetchPokemons, IPokemon, IPokemonSpecies } from '../models/models';
+import { pokemonSpeciesSlice } from '../reducers/pokemonSpeciesSlice';
 
 const instance = axios.create({
   baseURL: 'https://pokeapi.co/api/v2',
@@ -27,16 +28,6 @@ export const fetchPokemon = (name: string) => async (dispatch: Dispatch) => {
   }
 };
 
-export const fetchPokemonSpecies = (name: string) => async (dispatch: Dispatch) => {
-  try {
-    dispatch(pokemonSlice.actions.pokemonsPending());
-    const response = await instance.get<IPokemonSpecies>(`/pokemon-species/${name}/`);
-    dispatch(pokemonSlice.actions.fetchPokemonSpeciesSuccess(response.data));
-  } catch (e) {
-    dispatch(pokemonSlice.actions.pokemonSpeciesError(e));
-  }
-};
-
 export const fetchSearchPokemon = (name: string) => async (dispatch: Dispatch) => {
   try {
     dispatch(pokemonSlice.actions.pokemonsPending());
@@ -44,52 +35,10 @@ export const fetchSearchPokemon = (name: string) => async (dispatch: Dispatch) =
       const foundedPokemon = await instance.get<IPokemon>(`/pokemon/${name}`);
       const foundedSpecies = await instance.get<IPokemonSpecies>(`/pokemon-species/${name}/`);
       dispatch(pokemonSlice.actions.fetchSearchPokemonSuccess(foundedPokemon.data));
-      dispatch(pokemonSlice.actions.fetchPokemonSpeciesSuccess(foundedSpecies.data));
+      dispatch(pokemonSpeciesSlice.actions.fetchPokemonSpeciesSuccess(foundedSpecies.data));
     }
   } catch (e) {
     dispatch(pokemonSlice.actions.searchPokemonError(e));
-  }
-};
-
-export const fetchPokemonEvolutionChain = (name: string, url: string = '') => async (dispatch: Dispatch) => {
-  try {
-    dispatch(pokemonSlice.actions.pokemonsPending());
-    const evolutionPokemon = [];
-    if (url) {
-      const evolutionChain = await instance.get<IFetchEvolutionChain>(url);
-      const handleNameSpecies = (evolves: IEvolvesTo) => {
-        const { species, evolves_to: evolvesTo, evolution_details: evolutionDetails } = evolves;
-        let namesPokemons = [
-          {
-            name: species.name,
-            level: 0,
-            trigger: evolutionDetails[0]?.trigger?.name,
-            held_item: evolutionDetails[0]?.held_item?.name,
-          },
-        ];
-        if (evolutionDetails.length) {
-          namesPokemons[0].level = evolutionDetails[0].min_level;
-        }
-        evolvesTo.forEach((evolve: IEvolvesTo) => {
-          namesPokemons = namesPokemons.concat(handleNameSpecies(evolve));
-        });
-        return namesPokemons;
-      };
-      const pokemons = handleNameSpecies(evolutionChain.data.chain);
-      for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = await instance.get(`/pokemon/${pokemons[i].name}`);
-        const species = await instance.get<IPokemonSpecies>(`/pokemon-species/${pokemons[i].name}/`);
-        evolutionPokemon.push(pokemon.data);
-        dispatch(pokemonSlice.actions.fetchPokemonSpeciesSuccess(species.data));
-      }
-    }
-    if (evolutionPokemon.length > 1) {
-      dispatch(pokemonSlice.actions.fetchPokemonEvolutionChainSuccess(evolutionPokemon));
-    } else {
-      dispatch(pokemonSlice.actions.fetchPokemonEvolutionChainSuccess([]));
-    }
-  } catch (e) {
-    dispatch(pokemonSlice.actions.pokemonSpeciesError(e.message));
   }
 };
 
