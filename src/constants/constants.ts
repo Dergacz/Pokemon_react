0693c0b74048c757622193debdedbc374fef3ef7
Stas <sats96dergach@gamil.com>
2023-02-27 15:54:21 +0100

    add constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,10 @@
 export const DEFAULT_COLOR = 'rgba(0,0,0, .4);';
 export const ICE_COLOR = '#9AD6DF';
+export const ELECTRIC_COLOR = '#F9CF30';
 export const WHITE = 'white';
+export const YELLOW = 'yellow';
 export const FILTER_BY_TYPE = 'Filter by type';
+export const EN = 'en';
 export const REDUCTION_TITLES = [
   { title: 'hp', reduction: 'hp' },
   { title: 'attack', reduction: 'atk' },
