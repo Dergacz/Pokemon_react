40323eb7a0b2f057950a28af9df1bdd9e69ae2d2
Stas <sats96dergach@gamil.com>
2023-02-27 15:55:11 +0100

    fix yellow color

--- utils/setColor.ts
+++ utils/setColor.ts
@@ -4,6 +4,9 @@ export const setColor = (color: string) => {
   if (color === constants.WHITE) {
     return constants.ICE_COLOR;
   }
+  if (color === constants.YELLOW) {
+    return constants.ELECTRIC_COLOR;
+  }
   if (color) {
     return color;
   }
